1c4bece52d1c48c03b935bd75a3440d2779c1cf9
SarahMinich <minich.sarah@gmail.com>
2019-09-26 13:08:03 +0200

    Fichier de run
    Amélioration du fichier de run et mise en place de fichiers de résultats

--- easy_run_fptas.py
+++ easy_run_fptas.py
@@ -2,21 +2,48 @@
 import tools
 import time
 import fptas
+from improved_fptas import improved_FPTAS
+import datetime
 
 if __name__ == '__main__':
-    dir_path_for_inputs = "C:\\Users\\sarah\\Documents\\These\\Sub-problems\\2__tree-merging__Cmax\\4-Shared_programmation\\tree_structure\\inputs"
-    filename = "H3-nbT5-001"
-    my_input = tools.load_json_instance_from(dir_path_for_inputs, filename)
-
-    number_of_tiles = 5
-    number_of_iterations = 3
+    dir_path_for_inputs = "C:\\Users\\sarah\\Documents\\These\\Sub-problems\\2__tree-merging__Cmax\\4-Shared_programmation\\tree_structure\\inputs\\200-tiles"
+    root = "H9-nbT200-"
     epsilon = 0.1
- 
-    print("Beginning of the run.")
-    start = time.time()
-    (Cmax, number_of_generated_states) = fptas.FPTAS(my_input, epsilon/10)
-    stop = time.time()
-    duration = stop - start
-    print("Time needed : ", duration)
-    print(f"Cmax =  {Cmax}; number of states generated = {number_of_generated_states}")
+    path = f"results/txt/results_200-tiles_{epsilon}-epsilon.txt"  
+
+    print(f"Beginning of the run at {datetime.datetime.now()}")
+
+    for i in range(1, 101): # ! Upper bound not included
+        if 1 <= i <= 9:
+            filename = root + "00" + str(i)
+        elif 10 <= i <= 99:
+            filename = root + "0" + str(i)
+        else:
+            filename = root + str(i)
+
+
+        Cmax1 = number_of_generated_states1 = 0
+        Cmax2 = number_of_generated_states2 = 0
+        my_input = tools.load_json_instance_from(dir_path_for_inputs, filename)
+        
+        print(f"input number : {filename} at {datetime.datetime.now()}")
+
+        start1 = time.time()
+        (Cmax1, number_of_generated_states1) = fptas.FPTAS(my_input, epsilon)
+        stop1 = time.time()
+        print("Time needed for FPTAS: ", stop1 - start1)
+        print(f"FPTAS : Cmax =  {Cmax1}; number of states generated = {number_of_generated_states1}")
+
+        start2 = time.time()
+        (Cmax2, number_of_generated_states2) =  improved_FPTAS(my_input, epsilon)
+        stop2 = time.time()
+        print("Time needed for iFPTAS: ", stop2 - start2)
+        print(f"iFPTAS : Cmax =  {Cmax2}; number of states generated = {number_of_generated_states2}")
+        
+        my_str = f"\n{filename};{Cmax1};{stop1-start1};{number_of_generated_states1};{Cmax2};{stop2-start2};{number_of_generated_states2};"
+        file = open(path, "a")
+        file.write(my_str) 
+        file.close()
+
+    print(f"End of the run at {datetime.datetime.now()}")
     
