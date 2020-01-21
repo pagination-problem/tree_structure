7cb42a20619a01e227238f1961696e0284bc226c
SarahMinich <minich.sarah@gmail.com>
2020-01-21 10:27:56 +0100

    deleted one test function because it was useless
    As we deleted a lot of functions in Tile, testing them was not necessary anymore

--- test_type_tile.py
+++ test_type_tile.py
@@ -25,29 +25,29 @@ def creation_of_a_tile():
 
     print("creation_of_a_tile : everything is fine.\n")
 
-def creation_of_three_different_tiles():
-    size_s1 = 4
-    size_s2 = 2
-    sum_size_s1_s2 = size_s1 + size_s2
+# def creation_of_three_different_tiles():
+#     size_s1 = 4
+#     size_s2 = 2
+#     sum_size_s1_s2 = size_s1 + size_s2
 
-    s1 = Symbol(1, 4)
-    s2 = Symbol(2, 2)
-    s3 = Symbol(3, 7)
-    s4 = Symbol(4, 1)
+#     s1 = Symbol(1, 4)
+#     s2 = Symbol(2, 2)
+#     s3 = Symbol(3, 7)
+#     s4 = Symbol(4, 1)
 
-    t1 = Tile({s1, s2})
-    t2 = Tile({s2, s4})
-    t3 = Tile({s3, s2, s1})
+#     t1 = Tile({s1, s2})
+#     t2 = Tile({s2, s4})
+#     t3 = Tile({s3, s2, s1})
 
-    assert t1 != t2, "It says that the two tiles are identical : problem"
-    assert t1.size() == sum_size_s1_s2, "It says that the size of t1 != 6 : problem"
-    assert t1.is_included_in(t3), "It says that the symbols of t1 are not included in t3 : problem"
-    assert t1.is_included_in_at_least_one_tile_of_the_set({t2, t3}), "It says that t1 is not included in t3 : problem"
-    assert t1.is_included_in_at_least_one_tile_of_the_set({t2}) == False, "It says that t1 is included in t2 : problem"
-    assert t3.includes_at_least_a_tile_of_the_set({t1}), "It says that t3 does not include t1 : problem"
-    assert t3.includes_at_least_a_tile_of_the_set({t2}) == False, "It says that t3 includes t2 : problem"
+#     assert t1 != t2, "It says that the two tiles are identical : problem"
+#     assert t1.size() == sum_size_s1_s2, "It says that the size of t1 != 6 : problem"
+#     assert t1.is_included_in(t3), "It says that the symbols of t1 are not included in t3 : problem"
+#     assert t1.is_included_in_at_least_one_tile_of_the_set({t2, t3}), "It says that t1 is not included in t3 : problem"
+#     assert t1.is_included_in_at_least_one_tile_of_the_set({t2}) == False, "It says that t1 is included in t2 : problem"
+#     assert t3.includes_at_least_a_tile_of_the_set({t1}), "It says that t3 does not include t1 : problem"
+#     assert t3.includes_at_least_a_tile_of_the_set({t2}) == False, "It says that t3 includes t2 : problem"
     
-    print("creation_of_three_different_tiles : everything is fine.\n")       
+#     print("creation_of_three_different_tiles : everything is fine.\n")       
 
 def test_of_getting_the_leaf_out_of_a_tile():
     
