d22286b2e14130a4fe01405f3547b4c39a3fa45e
laowantong <laowantong@users.noreply.github.com>
2020-01-23 21:12:16 +0100

    add a minimal Instance class

--- instance.py
+++ instance.py
@@ -1,18 +1,40 @@
-from typing import Dict
-from symbol import Symbol
-from tile import Tile
 import json
+from pathlib import Path
+from symbol import Symbol
+from typing import Dict
+
+from tile import Tile
 
 
-def deserialize(json_instance: str) -> Dict:
-    """
-    A thin wrapper around the deserialization of an instance's JSON dump.
-    
-    The symbols and the tiles are converted into the appropriate objects.
-    The other fields, if any, are returned untouched.
-    """
-    data = json.loads(json_instance)
-    d = {s[0]: Symbol(*s) for s in zip(data.pop("symbol_indexes"), data.pop("symbol_sizes"))}
-    data["symbols"] = frozenset(d.values())
-    data["tiles"] = frozenset(Tile([d[i] for i in t]) for t in data.pop("tiles"))
-    return data
+class Instance:
+
+    @staticmethod
+    def deserialize(json_path: Path) -> Dict:
+        """
+        A thin wrapper around the deserialization of an instance's JSON dump.
+        
+        The symbols and the tiles are converted into the appropriate objects.
+        The other fields, if any, are returned untouched.
+        """
+        data = json.loads(json_path.read_text())
+        d = {s[0]: Symbol(*s) for s in zip(data.pop("symbol_indexes"), data.pop("symbol_sizes"))}
+        data["name"] = str(json_path)
+        data["symbols"] = frozenset(d.values())
+        data["tiles"] = frozenset(Tile([d[i] for i in t]) for t in data.pop("tiles"))
+        return data
+
+    def __init__(self, json_path: Path):
+        data = Instance.deserialize(json_path)
+        self.height = data["height"]
+        self.symbol_size_bound = data["symbol_size_bound"]
+        self.opt_tiles_m1 = data["opt_tiles_m1"]
+        self.opt_tiles_m2 = data["opt_tiles_m2"]
+        self.opt_value = data["opt_value"]
+        self.symbols_on_m1 = data["symbols_on_m1"]
+        self.symbols_on_m2 = data["symbols_on_m2"]
+        self.name = data["name"]
+        self.symbols = data["symbols"]
+        self.tiles = data["tiles"]
+
+        self.symbol_size_sum = sum(symbol.size for symbol in self.symbols)
+        self.tile_count = len(self.tiles)
--- tests/test_instance.py
+++ tests/test_instance.py
@@ -1,37 +1,20 @@
 import context
 
-from instance import deserialize
+from instance import Instance
 from symbol import Symbol
 from tile import Tile
-
+from pathlib import Path
 
 def test_deserialize():
-    text = """
-        {
-            "height": 3,
-            "symbol_size_bound": 9,
-            "opt_value": null,
-            "opt_tiles_m1": [],
-            "symbols_on_m1": [],
-            "opt_tiles_m2": [],
-            "symbols_on_m2": [],
-            "tiles": [
-                [1, 2, 5, 10],
-                [1, 2, 5, 11],
-                [1, 3, 7, 14],
-                [1, 2, 4, 9],
-                [1, 3, 7, 15]
-            ],
-            "symbol_indexes": [1, 2, 3, 4, 5, 7, 9, 10, 11, 14, 15],
-            "symbol_sizes": [0, 5, 3, 1, 2, 6, 2, 1, 4, 3, 5]
-        }
-    """
     expected = {
         "height": 3,
         "symbol_size_bound": 9,
         "opt_tiles_m1": [],
         "opt_tiles_m2": [],
         "opt_value": None,
+        "symbols_on_m1": [],
+        "symbols_on_m2": [],
+        "name": "tests/input/H3-nbT5-001.json",
         "symbols": frozenset(
             {
                 Symbol(1, 0),
@@ -47,8 +30,6 @@ def test_deserialize():
                 Symbol(15, 5),
             }
         ),
-        "symbols_on_m1": [],
-        "symbols_on_m2": [],
         "tiles": frozenset(
             {
                 Tile([Symbol(1, 0), Symbol(2, 5), Symbol(5, 2), Symbol(10, 1)]),
@@ -59,6 +40,6 @@ def test_deserialize():
             }
         ),
     }
-    actual = deserialize(text)
+    actual = Instance.deserialize(Path("tests/input/H3-nbT5-001.json"))
     print(actual)
     assert expected == actual
