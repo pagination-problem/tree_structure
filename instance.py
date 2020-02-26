f41ed74d7ff6e5f925a5bba57fca2a0dfcd8e82c
laowantong <laowantong@users.noreply.github.com>
2020-02-26 17:32:24 +0100

    modify and extend Instance class interface (cf. #2)
    Methods:
    - remove .deserialize()
    - add .get_data()
    - add .get_json()
    - add.dump_json()
    
    An instance can now be intialized with either a path towards a JSON file, or a dictionary whose structure is the same as obtained via a json.loads() deserialization.

--- instance.py
+++ instance.py
@@ -1,30 +1,33 @@
 import json
 from pathlib import Path
-from symbol import Symbol
-from typing import Dict
+from typing import Dict, Union
+
+from regex import sub
 
 from tile import Tile
+from symbol import Symbol
+
+
+def unwrap_int_list(m):
+    return f"[{', '.join(m.captures('n'))}]"
 
 
 class Instance:
+    def __init__(self, path_or_data: Union[Path, Dict]):
 
-    @staticmethod
-    def deserialize(json_path: Path) -> Dict:
-        """
-        A thin wrapper around the deserialization of an instance's JSON dump.
-        
-        The symbols and the tiles are converted into the appropriate objects.
-        The other fields, if any, are returned untouched.
-        """
-        data = json.loads(json_path.read_text())
+        # Create a new dictionary from the data
+        if isinstance(path_or_data, Path):
+            data = json.loads(path_or_data.read_text())
+            data["name"] = str(path_or_data)
+        else:
+            data = path_or_data.copy()  # avoid mutating the input data
+
+        # Convert raw symbols and tiles (simple lists of integers) into appropriate objects
         d = {s[0]: Symbol(*s) for s in zip(data.pop("symbol_indexes"), data.pop("symbol_sizes"))}
-        data["name"] = str(json_path)
         data["symbols"] = frozenset(d.values())
         data["tiles"] = frozenset(Tile([d[i] for i in t]) for t in data.pop("tiles"))
-        return data
 
-    def __init__(self, json_path: Path):
-        data = Instance.deserialize(json_path)
+        # Populate the other fields without touching the raw values
         self.height = data["height"]
         self.symbol_size_bound = data["symbol_size_bound"]
         self.opt_tiles_m1 = data["opt_tiles_m1"]
@@ -32,9 +35,32 @@ class Instance:
         self.opt_value = data["opt_value"]
         self.symbols_on_m1 = data["symbols_on_m1"]
         self.symbols_on_m2 = data["symbols_on_m2"]
-        self.name = data["name"]
+        self.name = data.get("name")
         self.symbols = data["symbols"]
         self.tiles = data["tiles"]
 
+        # Add some calculated attributes
         self.symbol_size_sum = sum(symbol.size for symbol in self.symbols)
         self.tile_count = len(self.tiles)
+
+    def get_data(self):
+        return {
+            "height": self.height,
+            "symbol_size_bound": self.symbol_size_bound,
+            "opt_value": self.opt_value,
+            "opt_tiles_m1": self.opt_tiles_m1,
+            "symbols_on_m1": self.symbols_on_m1,
+            "opt_tiles_m2": self.opt_tiles_m2,
+            "symbols_on_m2": self.symbols_on_m2,
+            "symbol_indexes": [s.index for s in sorted(self.symbols)],
+            "symbol_sizes": [s.size for s in sorted(self.symbols)],
+            "tiles": sorted(sorted(s.index for s in t.symbols) for t in self.tiles),
+        }
+
+    def get_json(self) -> str:
+        text = json.dumps(self.get_data(), indent=2)
+        text = sub(r"\[\s+((?P<n>\d+),?\s+)+\]", unwrap_int_list, text)
+        return text
+
+    def dump_json(self, json_path: Path) -> None:
+        json_path.write_text(self.get_json())
--- tests/test_instance.py
+++ tests/test_instance.py
@@ -1,45 +1,58 @@
-import context
-
-from instance import Instance
-from symbol import Symbol
-from tile import Tile
+import json
 from pathlib import Path
 
-def test_deserialize():
-    expected = {
-        "height": 3,
-        "symbol_size_bound": 9,
-        "opt_tiles_m1": [],
-        "opt_tiles_m2": [],
-        "opt_value": None,
-        "symbols_on_m1": [],
-        "symbols_on_m2": [],
-        "name": "tests/input/H3-nbT5-001.json",
-        "symbols": frozenset(
-            {
-                Symbol(1, 0),
-                Symbol(2, 5),
-                Symbol(3, 3),
-                Symbol(4, 1),
-                Symbol(5, 2),
-                Symbol(7, 6),
-                Symbol(9, 2),
-                Symbol(10, 1),
-                Symbol(11, 4),
-                Symbol(14, 3),
-                Symbol(15, 5),
-            }
-        ),
-        "tiles": frozenset(
-            {
-                Tile([Symbol(1, 0), Symbol(2, 5), Symbol(5, 2), Symbol(10, 1)]),
-                Tile([Symbol(1, 0), Symbol(2, 5), Symbol(5, 2), Symbol(11, 4)]),
-                Tile([Symbol(1, 0), Symbol(3, 3), Symbol(7, 6), Symbol(14, 3)]),
-                Tile([Symbol(1, 0), Symbol(2, 5), Symbol(4, 1), Symbol( 9, 2)]),
-                Tile([Symbol(1, 0), Symbol(3, 3), Symbol(7, 6), Symbol(15, 5)]),
-            }
-        ),
-    }
-    actual = Instance.deserialize(Path("tests/input/H3-nbT5-001.json"))
-    print(actual)
-    assert expected == actual
+import context
+from instance import Instance
+from tile import Tile
+from symbol import Symbol
+
+
+def test_init_with_json_path():
+    instance = Instance(Path("tests/input/H3-nbT5-001.json"))
+    assert instance.height == 3
+    assert instance.symbol_size_bound == 9
+    assert instance.opt_tiles_m1 == []
+    assert instance.opt_tiles_m2 == []
+    assert instance.opt_value == None
+    assert instance.symbols_on_m1 == []
+    assert instance.symbols_on_m2 == []
+    assert instance.name == "tests/input/H3-nbT5-001.json"
+    assert instance.symbols == frozenset(
+        {
+            Symbol(1, 0),
+            Symbol(2, 5),
+            Symbol(3, 3),
+            Symbol(4, 1),
+            Symbol(5, 2),
+            Symbol(7, 6),
+            Symbol(9, 2),
+            Symbol(10, 1),
+            Symbol(11, 4),
+            Symbol(14, 3),
+            Symbol(15, 5),
+        }
+    )
+    assert instance.tiles == frozenset(
+        {
+            Tile([Symbol(1, 0), Symbol(2, 5), Symbol(5, 2), Symbol(10, 1)]),
+            Tile([Symbol(1, 0), Symbol(2, 5), Symbol(5, 2), Symbol(11, 4)]),
+            Tile([Symbol(1, 0), Symbol(3, 3), Symbol(7, 6), Symbol(14, 3)]),
+            Tile([Symbol(1, 0), Symbol(2, 5), Symbol(4, 1), Symbol(9, 2)]),
+            Tile([Symbol(1, 0), Symbol(3, 3), Symbol(7, 6), Symbol(15, 5)]),
+        }
+    )
+
+
+def test_round_trip():
+    path = Path("tests/input/H3-nbT5-001.json")
+    text = path.read_text().strip()
+    data = json.loads(text)
+    instance_1 = Instance(data)
+    instance_2 = Instance(path)
+    json_1 = instance_1.get_json()
+    print(json_1)
+    json_2 = instance_2.get_json()
+    print(json_2)
+    assert json_1 == json_2
+    assert json_1 == text
+
