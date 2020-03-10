20ab9c26264e9005a195a8481367852cf50973b3
laowantong <laowantong@users.noreply.github.com>
2020-03-10 16:08:32 +0100

    make possible tiles of unequal lengths

--- instance_maker.py
+++ instance_maker.py
@@ -1,30 +1,46 @@
 import random
 
 
-def ancestors(node):
-    """Return all the ancestors of a node in a complete binary tree, including itself."""
-    result = [node]
-    while node:
-        node //= 2
-        result.append(node)
-    return result[::-1]
+class TrivialPathError(Exception):
+    ...
 
 
 class MakeInstance:
-    def __init__(self, height):
-        self.leaves = list(range(2 ** (height - 1), 2 ** height))
+    def __init__(self, height, arity=2):
+        self.leaves = list(range(arity ** (height - 1), arity ** height))
+        self.arity = arity
+        self.max_sample_size = arity ** height
 
-    def create_sample_paths(self, sample_size):
+    def ancestors(self, node):
+        """Return all the ancestors of a node in a complete binary tree, including itself."""
+        result = [node]
+        while node:
+            node //= self.arity
+            result.append(node)
+        return result[::-1]
+
+    def create_path_sample(self, sample_size):
         """Create all paths from the root to a random sample of leaves in a binary tree."""
-        assert sample_size <= len(self.leaves), "The sample size should be at most 2 ** height."
-        self.paths = [ancestors(leaf) for leaf in random.sample(self.leaves, sample_size)]
+        assert sample_size <= self.max_sample_size
+        self.paths = [self.ancestors(leaf) for leaf in random.sample(self.leaves, sample_size)]
+
+    def remove_random_nodes(self, kill_rate):
+        """Suppress a certain percentage of the distinct nodes occurring in self.paths."""
+        if not kill_rate:
+            return
+        remaining_nodes = set.union(*map(set, self.paths))
+        to_kill = set(random.sample(remaining_nodes, round(len(remaining_nodes) * kill_rate)))
+        for path in self.paths:
+            path[:] = [node for node in path if node not in to_kill]
+            if len(path) <= 1:
+                raise TrivialPathError
 
     def renumber_symbols(self):
         """Update self.paths with all their nodes renumbered consecutively."""
         remaining_nodes = set.union(*map(set, self.paths))
+        self.node_count = len(remaining_nodes)
         renumber_node = dict((old, new) for (new, old) in enumerate(remaining_nodes)).get
         self.paths = [list(map(renumber_node, path)) for path in self.paths]
-        self.node_count = len(remaining_nodes)
 
     def create_random_weights(self, max_weight):
         """Return a list of n random weights, where n is the number of distinct nodes in self.paths."""
--- tests/test_instance_maker.py
+++ tests/test_instance_maker.py
@@ -3,20 +3,17 @@ import random
 import pytest
 
 import context
-from instance_maker import MakeInstance
+from instance_maker import MakeInstance, TrivialPathError
+
+maker = MakeInstance(height=4)
+assert maker.leaves == [8, 9, 10, 11, 12, 13, 14, 15]
+with pytest.raises(AssertionError):
+    maker.create_path_sample(sample_size=100)
 
 
-def test_call_sequence():
-
-    maker = MakeInstance(height=4)
-    assert maker.leaves == [8, 9, 10, 11, 12, 13, 14, 15]
-
+def test_normal_sequence():
     random.seed(42)
-
-    with pytest.raises(AssertionError):
-        maker.create_sample_paths(sample_size=100)
-
-    maker.create_sample_paths(sample_size=5)
+    maker.create_path_sample(sample_size=5)
     assert maker.paths == [
         [0, 1, 2, 4, 9],
         [0, 1, 2, 4, 8],
@@ -25,16 +22,32 @@ def test_call_sequence():
         [0, 1, 3, 7, 15],
     ]
 
+    maker.remove_random_nodes(kill_rate=0.3)
+    assert maker.paths == [
+        [0, 4, 9],
+        [0, 4],
+        [0, 6, 13],
+        [0, 5, 10],
+        [0, 7, 15],
+    ]
+
     maker.renumber_symbols()
     assert maker.paths == [
-        [0, 1, 2, 4, 9],
-        [0, 1, 2, 4, 8],
-        [0, 1, 3, 6, 11],
-        [0, 1, 2, 5, 10],
-        [0, 1, 3, 7, 12],
+        [0, 1, 5],
+        [0, 1],
+        [0, 3, 7],
+        [0, 2, 6],
+        [0, 4, 8],
     ]
-    assert maker.node_count == 13
+    assert maker.node_count == 9
     assert maker.node_count == maker.paths[-1][-1] + 1
 
     maker.create_random_weights(max_weight=4)
-    assert maker.weights == [2, 2, 1, 1, 4, 1, 1, 1, 2, 2, 1, 2, 4]
+    assert maker.weights == [1, 4, 1, 1, 1, 2, 2, 1, 2]
+
+
+def test_removing_too_much_nodes():
+    random.seed(42)
+    maker.create_path_sample(sample_size=5)
+    with pytest.raises(TrivialPathError):
+        maker.remove_random_nodes(kill_rate=0.6)
