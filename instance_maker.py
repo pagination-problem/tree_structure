ebac760d521881c65075d4d77a9b002834e949cb
laowantong <laowantong@users.noreply.github.com>
2020-04-03 15:01:02 +0200

    add 3 parameters to instance generation (see #9)

--- instance_maker.py
+++ instance_maker.py
@@ -1,17 +1,28 @@
 import json
-from random import sample, randint, randrange, seed
+import sys
+from functools import reduce
 from hashlib import sha256
 from pathlib import Path
+from random import randint, randrange, sample, seed
 
 from goodies import data_to_json
 
 
-class MakeInstance:
-    def __init__(self, height, arity=2):
+class InstanceMaker:
+    def __init__(
+        self,
+        height,
+        arity=2,
+        tile_min_size=2,
+        common_symbol_min_count=0,
+        common_symbol_max_count=sys.maxsize,
+    ):
         self.leaves = list(range(arity ** (height - 1), arity ** height))
-        self.arity = arity
-        self.max_sample_weight = arity ** height
         self.height = height
+        self.arity = arity
+        self.tile_min_size = tile_min_size
+        self.common_symbol_min_count = common_symbol_min_count
+        self.common_symbol_max_count = common_symbol_max_count
 
     def ancestors(self, node):
         """Return all the ancestors of a node in a complete n-ary tree, including itself."""
@@ -24,22 +35,25 @@ class MakeInstance:
     def create_path_sample(self, leaf_rate):
         """Create all paths from the root to a random sample of leaves in a complete n-ary tree."""
         assert 0 <= leaf_rate <= 1
-        self.paths = [
+        self.paths = sorted(
             self.ancestors(leaf)
             for leaf in sample(self.leaves, round(len(self.leaves) * leaf_rate))
-        ]
+        )
 
     def remove_random_nodes(self, kill_rate):
         """
         Suppress a certain percentage (expressed as a floating-point value in [0, 1]) of the distinct
-        nodes occurring in self.paths, excluding the root and the leaves.
+        nodes occurring in self.paths.
         """
         if 0 < kill_rate <= 1:
             candidates = set.union(*map(set, self.paths))
             to_kill = set(sample(candidates, round(len(candidates) * kill_rate)))
             for path in self.paths:
                 path[:] = [node for node in path if node not in to_kill]
-            self.paths.sort()
+                if len(path) < self.tile_min_size:
+                    raise TileTooSmall
+            if len(set(map(tuple, self.paths))) != len(self.paths):
+                raise DuplicateTiles
 
     def renumber_symbols(self):
         """Update self.paths with all their nodes renumbered consecutively."""
@@ -47,6 +61,11 @@ class MakeInstance:
         self.node_count = len(remaining_nodes)
         renumber_node = dict((old, new) for (new, old) in enumerate(remaining_nodes)).get
         self.paths = [list(map(renumber_node, path)) for path in self.paths]
+        self.common_symbols = reduce(lambda s1, s2: set(s1).intersection(s2), self.paths)
+        if len(self.common_symbols) < self.common_symbol_min_count:
+            raise TooFewCommonSymbols
+        if len(self.common_symbols) > self.common_symbol_max_count:
+            raise TooManyCommonSymbols
 
     def create_random_weights(self, max_weight):
         """Return a list of n random weights, where n is the number of distinct nodes in self.paths."""
@@ -68,9 +87,26 @@ class MakeInstance:
             "height": self.height,  # seems useless
             "symbol_weight_bound": max(self.weights),  # may be less than symbol_weight_bound
             "symbol_count": self.node_count,
+            "common_symbols": sorted(self.common_symbols),
             "symbol_weights": self.weights,
-            "tiles": self.paths,
+            "tiles": sorted(self.paths),
         }
+
+
+class TileTooSmall(Exception):
+    ...
+
+
+class DuplicateTiles(Exception):
+    ...
+
+
+class TooManyCommonSymbols(Exception):
+    ...
+
+
+class TooFewCommonSymbols(Exception):
+    ...
 
 
 def dump_instances(config_path):
@@ -78,15 +114,38 @@ def dump_instances(config_path):
     directory = Path(cfg["output_directory"])
     directory.mkdir(parents=True, exist_ok=True)
     seed(cfg["seed"])
-    for i in range(1, cfg["instance_count"] + 1):
+    i = 1
+    column = 1
+    while i <= cfg["instance_count"]:
         height = randint(cfg["min_height"], cfg["max_height"])
-        maker = MakeInstance(height, cfg["arity"])
+        maker = InstanceMaker(
+            height,
+            cfg["arity"],
+            cfg["tile_min_size"],
+            cfg["common_symbol_min_count"],
+            cfg["common_symbol_max_count"],
+        )
         leaf_rate = randint(cfg["min_tile_percentage"], cfg["max_tile_percentage"]) / 100.0
         kill_rate = randint(cfg["min_kill_percentage"], cfg["max_kill_percentage"]) / 100.0
-        symbol_weight_bound = randint(cfg["min_symbol_weight_bound"], cfg["max_symbol_weight_bound"])
-        instance = maker(leaf_rate, kill_rate, symbol_weight_bound)
-        (directory / instance["name"]).write_text(data_to_json(instance))
-        print(".", end="" if i % 80 else "\n", flush=True)
+        symbol_weight_bound = randint(
+            cfg["min_symbol_weight_bound"], cfg["max_symbol_weight_bound"]
+        )
+        try:
+            instance = maker(leaf_rate, kill_rate, symbol_weight_bound)
+        except DuplicateTiles:
+            outcome = "D"
+        except TileTooSmall:
+            outcome = "S"
+        except TooFewCommonSymbols:
+            outcome = "F"
+        except TooManyCommonSymbols:
+            outcome = "M"
+        else:
+            (directory / instance["name"]).write_text(data_to_json(instance))
+            outcome = "."
+            i += 1
+        print(outcome, end="" if column % 80 else "\n", flush=True)
+        column += 1
 
 
 if __name__ == "__main__":
--- tests/test_instance_maker.py
+++ tests/test_instance_maker.py
@@ -3,109 +3,100 @@ import random
 import pytest
 
 import context
-from instance_maker import MakeInstance
+from instance_maker import (
+    InstanceMaker,
+    TileTooSmall,
+    TooFewCommonSymbols,
+    TooManyCommonSymbols,
+)
 
 
 def test_call_sequence():
-    maker = MakeInstance(height=4)
+    maker = InstanceMaker(height=4)
     assert maker.leaves == [8, 9, 10, 11, 12, 13, 14, 15]
 
     random.seed(42)
     maker.create_path_sample(leaf_rate=0.8)
+    print(maker.paths)
     assert maker.paths == [
-        [0, 1, 2, 4, 9],
         [0, 1, 2, 4, 8],
-        [0, 1, 3, 6, 13],
+        [0, 1, 2, 4, 9],
         [0, 1, 2, 5, 10],
-        [0, 1, 3, 7, 15],
+        [0, 1, 3, 6, 13],
         [0, 1, 3, 7, 14],
+        [0, 1, 3, 7, 15],
     ]
 
-    maker.remove_random_nodes(kill_rate=0.4)
+    maker.remove_random_nodes(kill_rate=0.1)
+    print(maker.paths)
     assert maker.paths == [
-        [0, 3, 7, 14],
-        [0, 3, 7, 15],
-        [0, 3, 13],
-        [0, 4, 8],
-        [0, 4, 9],
-        [0, 5, 10],
-    ]
-
-    maker.renumber_symbols()
-    assert maker.paths == [
+        [0, 1, 4, 8],
         [0, 1, 4, 9],
-        [0, 1, 4, 10],
-        [0, 1, 8],
-        [0, 2, 5],
-        [0, 2, 6],
-        [0, 3, 7],
-    ]
-    assert maker.node_count == 11
-
-    maker.create_random_weights(max_weight=4)
-    assert maker.weights == [1, 4, 1, 1, 1, 2, 2, 1, 2, 4, 2]
-
-
-def test_beyond_binary_trees():
-    maker = MakeInstance(height=3, arity=3)
-    assert maker.leaves == [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
-
-    random.seed(42)
-    maker.create_path_sample(leaf_rate=0.8)
-    assert maker.paths == [
-        [0, 1, 4, 12],
-        [0, 1, 3, 9],
-        [0, 1, 5, 17],
-        [0, 2, 8, 26],
-        [0, 2, 7, 23],
-        [0, 1, 3, 11],
-        [0, 2, 6, 20],
-        [0, 1, 3, 10],
-        [0, 2, 8, 24],
-        [0, 2, 6, 19],
-        [0, 1, 5, 15],
-        [0, 2, 8, 25],
-        [0, 1, 5, 16],
-        [0, 1, 4, 14],
-    ]
-
-    maker.remove_random_nodes(kill_rate=0.2)
-    assert maker.paths == [
-        [0, 1, 3, 9],
-        [0, 1, 3, 10],
-        [0, 1, 3, 11],
-        [0, 1, 5, 15],
-        [0, 1, 5, 16],
-        [0, 1, 5, 17],
-        [0, 1, 12],
-        [0, 1, 14],
-        [0, 6, 19],
-        [0, 6, 20],
-        [0, 7, 23],
-        [0, 8, 24],
-        [0, 8, 25],
-        [0, 8, 26],
+        [0, 1, 5, 10],
+        [0, 1, 3, 6, 13],
+        [0, 1, 3, 7, 14],
+        [0, 1, 3, 7, 15],
     ]
 
     maker.renumber_symbols()
+    print(maker.paths)
     assert maker.paths == [
-        [0, 1, 2, 7],
-        [0, 1, 2, 8],
-        [0, 1, 2, 9],
-        [0, 1, 3, 12],
-        [0, 1, 3, 13],
-        [0, 1, 3, 14],
-        [0, 1, 10],
-        [0, 1, 11],
-        [0, 4, 15],
-        [0, 4, 16],
-        [0, 5, 17],
-        [0, 6, 18],
-        [0, 6, 19],
-        [0, 6, 20],
+        [0, 1, 3, 7],
+        [0, 1, 3, 8],
+        [0, 1, 4, 9],
+        [0, 1, 2, 5, 10],
+        [0, 1, 2, 6, 11],
+        [0, 1, 2, 6, 12],
     ]
-    assert maker.node_count == 21
+    assert maker.node_count == 13
 
     maker.create_random_weights(max_weight=4)
     print(maker.weights)
-    assert maker.weights == [1, 2, 4, 2, 4, 3, 1, 2, 4, 3, 3, 2, 2, 3, 1, 1, 4, 1, 3, 3, 3]
+    assert maker.weights == [1, 1, 4, 1, 1, 1, 2, 2, 1, 2, 4, 2, 4]
+
+
+def test_beyond_binary_trees():
+    maker = InstanceMaker(height=3, arity=3)
+    assert maker.leaves == [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+
+
+def test_tile_too_small():
+    # Success for tile_min_size == 2
+    maker = InstanceMaker(height=4, tile_min_size=2)
+    random.seed(42)
+    maker.create_path_sample(leaf_rate=0.8)
+    maker.remove_random_nodes(kill_rate=0.3)
+    print(maker.paths)
+    assert maker.paths == [
+        [0, 4, 8],
+        [0, 4, 9],
+        [0, 5],
+        [0, 3, 6],
+        [0, 3, 7, 14],
+        [0, 3, 7, 15],
+    ]
+
+    # Failure for tile_min_size == 3
+    maker = InstanceMaker(height=4, tile_min_size=3)
+    random.seed(42)
+    maker.create_path_sample(leaf_rate=0.8)
+    with pytest.raises(TileTooSmall):
+        maker.remove_random_nodes(kill_rate=0.3)
+
+
+def test_too_few_common_symbols():
+    maker = InstanceMaker(height=4, tile_min_size=2, common_symbol_min_count=2)
+    random.seed(42)
+    maker.create_path_sample(leaf_rate=0.8)
+    maker.remove_random_nodes(kill_rate=0.3)  # see self.paths' value in test_tile_too_small()
+    with pytest.raises(TooFewCommonSymbols):
+        maker.renumber_symbols()
+
+
+def test_too_many_common_symbols():
+    maker = InstanceMaker(height=4, tile_min_size=2, common_symbol_max_count=0)
+    random.seed(42)
+    maker.create_path_sample(leaf_rate=0.8)
+    maker.remove_random_nodes(kill_rate=0.3)  # see self.paths' value in test_tile_too_small()
+    with pytest.raises(TooManyCommonSymbols):
+        maker.renumber_symbols()
