45819d5908a2972909c2248fe60f319529b5776c
laowantong <laowantong@users.noreply.github.com>
2020-05-08 19:36:53 +0200

    add (unified) backtracking for improved engine

--- solver_fptas.py
+++ solver_fptas.py
@@ -7,10 +7,8 @@ class Fptas(AbstractSolver):
     def set_engine_strategy(self, engine_name):
         if engine_name == "basic":
             self.launch_engine = self.basic_engine
-            self.retrieve_solution = self.retrieve_basic_solution
         elif engine_name == "improved":
             self.launch_engine = self.improved_engine
-            self.retrieve_solution = self.retrieve_improved_solution
 
     def set_instance(self, instance):
         self.may_reset_log()
@@ -40,47 +38,45 @@ class Fptas(AbstractSolver):
             self.may_log(states)
         return states
 
-    def retrieve_basic_solution(self):
-        """Backtrack the logged states to tell which tiles are assigned to which bins."""
-        log_result = self.log_result[:]
-        assert log_result
-        self.best_states = [min(log_result.pop(), key=lambda state: max(state[0], state[1]))]
-        while log_result:
-            bin1 = self.best_states[-1][0::2]
-            bin2 = self.best_states[-1][1::2]
-            states = log_result.pop()
-            for state in states:
-                if state[0::2] == bin1 or state[1::2] == bin2:
-                    self.best_states.append(state)
-                    break
-            else:
-                raise ValueError(f"Cannot match {self.best_states[-1]} in {states}.")
-        bin1 = set(t for (_, _, t, _) in self.best_states if t != NO_LAST_TILE)
-        bin2 = set(t for (_, _, _, t) in self.best_states if t != NO_LAST_TILE)
-        assert not bin1.intersection(bin2)
-        assert len(bin1.union(bin2)) == self.instance.tile_count
-        return (sorted(bin1), sorted(bin2))
-
     def improved_engine(self):
-        states = [(0, 0, NO_LAST_TILE, 2)]
+        states = [(0, 0, NO_LAST_TILE, 2, NO_LAST_TILE, NO_LAST_TILE)]
         self.may_log(states)
         last1 = NO_LAST_TILE
         for new in range(self.instance.tile_count):
             self.grid.reset()
-            for (w1, w2, last2, alpha) in states:
+            for (w1, w2, last2, alpha, top1, top2) in states:
                 if alpha == 1:
-                    self.grid.may_add_state((w1 + self.costs[new][last1], w2, last2, 1))
-                    self.grid.may_add_state((w1, w2 + self.costs[new][last2], last1, 2))
+                    self.grid.may_add_state((w1 + self.costs[new][last1], w2, last2, 1, new, top2))
+                    self.grid.may_add_state((w1, w2 + self.costs[new][last2], last1, 2, top1, new))
                 else:
-                    self.grid.may_add_state((w1 + self.costs[new][last2], w2, last1, 1))
-                    self.grid.may_add_state((w1, w2 + self.costs[new][last1], last2, 2))
+                    self.grid.may_add_state((w1 + self.costs[new][last2], w2, last1, 1, new, top2))
+                    self.grid.may_add_state((w1, w2 + self.costs[new][last1], last2, 2, top1, new))
             last1 = new
             states = self.grid.get_states()
             self.may_log(states)
         return states
 
-    def retrieve_improved_solution(self):
-        raise NotImplementedError
+    def retrieve_solution(self):
+        """Backtrack the logged states to tell which tiles are assigned to which bins."""
+        log_result = self.log_result[:]
+        assert log_result
+        self.best_states = [min(log_result.pop(), key=lambda state: max(state[0], state[1]))]
+        while log_result:
+            best_state = self.best_states[-1]
+            bin1 = [best_state[0], best_state[-2]]
+            bin2 = [best_state[1], best_state[-1]]
+            states = log_result.pop()
+            for state in states:
+                if bin1 == [state[0], state[-2]] or bin2 == [state[1], state[-1]]:
+                    self.best_states.append(state)
+                    break
+            else:
+                raise ValueError(f"Cannot match {self.best_states[-1]} in {states}.")
+        bin1 = set(state[-2] for state in self.best_states if state[-2] != NO_LAST_TILE)
+        bin2 = set(state[-1] for state in self.best_states if state[-1] != NO_LAST_TILE)
+        assert not bin1.intersection(bin2)
+        assert len(bin1.union(bin2)) == self.instance.tile_count
+        return (sorted(bin1), sorted(bin2))
 
 
 class Grid:
