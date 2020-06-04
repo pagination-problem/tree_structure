0a5fe3908c05fab623af81bd807d30bc84764ae6
SarahMinich <minich.sarah@gmail.com>
2020-06-04 14:21:44 +0200

    Merged the two generate_instances files

--- generate_instances.py
+++ generate_instances.py
@@ -14,6 +14,9 @@ from numpy import base_repr
 
 from goodies import data_to_json
 
+import numpy as np
+from scipy import optimize
+
 
 class InstanceMaker:
     def __init__(
@@ -23,6 +26,10 @@ class InstanceMaker:
         tile_min_size=2,
         common_symbol_min_count=0,
         common_symbol_max_count=sys.maxsize,
+        desired_cost_mean=20,
+        LB_standard_deviation=3,
+        UB_standard_deviation=8,
+        method="classic"
     ):
         self.leaves = list(range(arity ** (height - 1), arity ** height))
         self.height = height
@@ -30,6 +37,11 @@ class InstanceMaker:
         self.tile_min_size = tile_min_size
         self.common_symbol_min_count = common_symbol_min_count
         self.common_symbol_max_count = common_symbol_max_count
+        self.desired_cost_mean = desired_cost_mean
+        self.LB_standard_deviation = LB_standard_deviation
+        self.UB_standard_deviation = UB_standard_deviation
+        self.method = method #if method==classic, we generate instances without controlling the mean and the standard deviation. 
+                             #if method==stats (or anything else in fact), it means we want to control the stats
 
     def ancestors(self, node):
         """Return all the ancestors of a node in a complete n-ary tree, including itself."""
@@ -78,13 +90,130 @@ class InstanceMaker:
         """Return a list of n random weights, where n is the number of distinct nodes in self.paths."""
         self.weights = [1 + randrange(max_weight) for _ in range(self.node_count)]
 
+    def compute_occurrences_of_p_i(self, the_instance):
+        l = list()
+        p_i_occurrences = [0 for i in range(the_instance.symbol_count)]
+
+        for (new, new_tile) in enumerate(the_instance.tiles):
+            for last_tile in the_instance.tiles[:new]:
+                [l.append(symbol.index) for symbol in new_tile - last_tile]
+                
+            for s in l:
+                p_i_occurrences[s] = p_i_occurrences[s] + 1
+                
+            l = list()
+            
+        for new_tile in the_instance.tiles:
+            for symbol in new_tile:
+                p_i_occurrences[symbol.index] = p_i_occurrences[symbol.index] + 1
+                
+        return p_i_occurrences
+
+    def compute_coefficient_for_quadratic_var(self, the_instance):
+        try:
+            coeff_for_quadratic =  [[0 for y in range(the_instance.symbol_count)] for x in range(the_instance.symbol_count)]
+        except MemoryError:
+            print("System out of memory, now quitting")
+        else:
+            #traitement basé sur la première colonne de la matrice costs
+            for tile in the_instance.tiles:
+                for s1 in tile:
+                    for s2 in tile:
+                        if s1.index == s2.index:
+                            coeff_for_quadratic[s1.index][s2.index] = coeff_for_quadratic[s1.index][s2.index] + 1
+                        elif s1.index < s2.index:
+                            coeff_for_quadratic[s1.index][s2.index] = coeff_for_quadratic[s1.index][s2.index] + 2
+
+                            
+            # Traitement du reste de la matrice :
+            for (new, new_tile) in enumerate(the_instance.tiles):
+                for last_tile in the_instance.tiles[:new]:
+                    for s1 in new_tile - last_tile:
+                        for s2 in new_tile - last_tile:
+                            if s1.index == s2.index:
+                                coeff_for_quadratic[s1.index][s2.index] = coeff_for_quadratic[s1.index][s2.index] + 1
+                            elif s1.index < s2.index:
+                                coeff_for_quadratic[s1.index][s2.index] = coeff_for_quadratic[s1.index][s2.index] + 2
+    
+        return coeff_for_quadratic
+
+    def create_weight_according_to_mean_and_deviation(
+        self,
+        the_instance,
+        p_i_occurrences,
+        coeff_for_quadratic
+    ):
+        cell_count_in_costs_matrix = (the_instance.tile_count * (the_instance.tile_count + 1)) / 2
+        p_i_count = self.node_count
+
+        def f(x):
+            # The objective function is not important
+            return 1
+
+        def mean_constraint(x):
+            return np.dot(p_i_occurrences, x) - cell_count_in_costs_matrix * self.desired_cost_mean
+
+        def define_standard_deviation(x, coeff_for_quadratic):
+            res = self.desired_cost_mean * self.desired_cost_mean  * cell_count_in_costs_matrix
+            for l in range(p_i_count):
+                for c in range(l, p_i_count):
+                    res = res + coeff_for_quadratic[l][c] * x[l] * x[c]
+
+            for l in range(p_i_count):
+                res = res - 2 * self.desired_cost_mean * coeff_for_quadratic[l][l] * x[l]
+            
+            return res * (1/cell_count_in_costs_matrix)
+
+        def sd_constraint_1(x, coeff_for_quadratic=coeff_for_quadratic): #LB for the standard deviation
+            return define_standard_deviation(x, coeff_for_quadratic) - (self.LB_standard_deviation * self.LB_standard_deviation)
+
+        def sd_constraint_2(x, coeff_for_quadratic=coeff_for_quadratic): #UB for the standard deviation
+            return  (self.UB_standard_deviation * self.UB_standard_deviation) - define_standard_deviation(x, coeff_for_quadratic)
+        
+        cons = [{'type': 'eq', 'fun': mean_constraint},
+        {'type': 'ineq', 'fun': sd_constraint_1},
+        {'type': 'ineq', 'fun': sd_constraint_2}]
+
+        bnds = list()
+        for i in range(p_i_count):
+            bnds.append([0, np.inf])
+
+        variables =  np.array([0 for y in range(p_i_count)])
+        value_distrib = optimize.minimize(f, variables, method="SLSQP", bounds=bnds, constraints=cons)
+        # if we want more control over the possible values for the p_i, we will need to store the values read
+        # in this document line 294 : cfg["min_symbol_weight_bound"], cfg["max_symbol_weight_bound"]
+    
+        return value_distrib.x
+
     def __call__(self, leaf_rate, kill_rate, symbol_weight_bound):
         self.create_path_sample(leaf_rate)
         self.remove_random_nodes(kill_rate)
         self.renumber_symbols()
-        self.create_random_weights(symbol_weight_bound)
-        identifier = f"{self.paths},{self.weights}".encode("utf8")
 
+        if self.method == "classic":
+            self.create_random_weights(symbol_weight_bound)
+        else:
+            self.weights = [0 for i in range(self.node_count)]
+            temporary_dict = {
+                "name": "temp",
+                "height": self.height,  # seems useless
+                "symbol_weight_bound": max(self.weights),  # may be less than symbol_weight_bound
+                "symbol_count": self.node_count,
+                "common_symbols": sorted(self.common_symbols),
+                "symbol_weights": self.weights,
+                "cost_mean": self.desired_cost_mean,
+                "cost_standard_deviation": "N/A",
+                "tiles": sorted(self.paths),
+                "costs": "N/A"
+            }
+            temporary_instance = Instance(temporary_dict)
+
+            p_i_occurrences = self.compute_occurrences_of_p_i(temporary_instance)
+            coefficients_for_quadratic_var = self.compute_coefficient_for_quadratic_var(temporary_instance)
+            temp_weights = self.create_weight_according_to_mean_and_deviation(temporary_instance, p_i_occurrences, coefficients_for_quadratic_var)
+            self.weights =  [temp_weights[i] for i in range(self.node_count)] #tranformation necessary because json doesnt know NDarray of numpy
+            
+        identifier = f"{self.paths},{self.weights}".encode("utf8")
         returned_dict = {
             "name": "h={height:02}_t={tiles:03}_s={symbols:03}_m={max_weight:02}__{hash_value}.json".format(
                 height=self.height,
@@ -103,7 +232,7 @@ class InstanceMaker:
             "tiles": sorted(self.paths),
             "costs": "N/A"
         }
-
+        
         temporary_instance = Instance(returned_dict)
 
         costs = [[tile.weight] * (temporary_instance.tile_count + 1) for tile in temporary_instance.tiles]
@@ -154,6 +283,10 @@ def dump_instances(config_path):
             cfg["tile_min_size"],
             cfg["common_symbol_min_count"],
             cfg["common_symbol_max_count"],
+            cfg["desired_cost_mean"],
+            cfg["LB_standard_deviation"],
+            cfg["UB_standard_deviation"],
+            cfg["method"]
         )
         leaf_rate = randint(cfg["min_tile_percentage"], cfg["max_tile_percentage"]) / 100.0
         kill_rate = randint(cfg["min_kill_percentage"], cfg["max_kill_percentage"]) / 100.0
@@ -179,7 +312,7 @@ def dump_instances(config_path):
 
 
 if __name__ == "__main__":
-    filename = "instances/sarah/test_config.json" if len(sys.argv) <= 1 else sys.argv[1]
+    filename = "instances/sarah/config_for_stats.json" if len(sys.argv) <= 1 else sys.argv[1]
     # alternatively:  "instances/snapshots.json"
     dump_instances(filename)
-    
+
--- tests/test_generate_instances.py
+++ tests/test_generate_instances.py
@@ -10,6 +10,8 @@ from generate_instances import (
     TooManyCommonSymbols,
 )
 
+from instance import Instance
+import numpy as np
 
 def test_call_sequence():
     maker = InstanceMaker(height=4)
@@ -100,3 +102,138 @@ def test_too_many_common_symbols():
     maker.remove_random_nodes(kill_rate=0.3)  # see self.paths' value in test_tile_too_small()
     with pytest.raises(TooManyCommonSymbols):
         maker.renumber_symbols()
+
+def test_compute_occurrences_of_p_i():
+    the_dict = {
+        "name": "h=03_t=005_s=011_m=06.json",
+        "height": 3,
+        "symbol_weight_bound": 6,
+        "symbol_count": 11,
+        "symbol_weights": [0, 5, 3, 1, 2, 6, 2, 1, 4, 3, 5],
+        "cost_mean": 9.933333333333334,
+        "cost_standard_deviation": 3.785351884420904,
+        "tiles": [
+            [0, 1, 3, 6],
+            [0, 1, 4, 7],
+            [0, 1, 4, 8],
+            [0, 2, 5, 9],
+            [0, 2, 5, 10]
+        ],
+        "costs": [
+            [8, 8, 8, 8, 8, 8],
+            [3, 8, 8, 8, 8, 8],
+            [6, 4, 11, 11, 11, 11],
+            [12, 12, 12, 12, 12, 12],
+            [14, 14, 14, 5, 14, 14]
+        ]
+    }
+    maker = InstanceMaker(height=the_dict["height"], tile_min_size=2, common_symbol_max_count=1)
+    the_instance = Instance(the_dict)
+    computed_occurrences = maker.compute_occurrences_of_p_i(the_instance)
+
+    assert computed_occurrences == [5, 3, 8, 1, 4, 8, 1, 2, 3, 4, 5]
+
+def test_compute_coefficient_for_quadratic_var():
+    the_dict = {
+    "name": "h=03_t=005_s=011_m=06.json",
+    "height": 3,
+    "symbol_weight_bound": 6,
+    "symbol_count": 11,
+    "symbol_weights": [0, 5, 3, 1, 2, 6, 2, 1, 4, 3, 5],
+    "cost_mean": 9.933333333333334,
+    "cost_standard_deviation": 3.785351884420904,
+    "tiles": [
+        [0, 1, 3, 6],
+        [0, 1, 4, 7],
+        [0, 1, 4, 8],
+        [0, 2, 5, 9],
+        [0, 2, 5, 10]
+    ],
+    "costs": [
+        [8, 8, 8, 8, 8, 8],
+        [3, 8, 8, 8, 8, 8],
+        [6, 4, 11, 11, 11, 11],
+        [12, 12, 12, 12, 12, 12],
+        [14, 14, 14, 5, 14, 14]
+        ]
+    }
+    maker = InstanceMaker(height=the_dict["height"], tile_min_size=2, common_symbol_max_count=1)
+    the_instance = Instance(the_dict)
+    computed_coefficients = maker.compute_coefficient_for_quadratic_var(the_instance)
+    assert computed_coefficients == [
+            [5, 6, 4, 2, 4, 4, 2, 2, 2, 2, 2],
+            [0, 3, 0, 2, 4, 0, 2, 2, 2, 0, 0],
+            [0, 0, 8, 0, 0, 16, 0, 0, 0, 8, 8],
+            [0, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 0, 4, 0, 0, 4, 4, 0, 0],
+            [0, 0, 0, 0, 0, 8, 0, 0, 0, 8, 8],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5]
+        ]
+
+def test_create_weight_according_to_mean_and_deviation():
+    the_dict = {
+    "name": "h=03_t=005_s=011_m=06.json",
+    "height": 3,
+    "symbol_weight_bound": 6,
+    "symbol_count": 11,
+    "symbol_weights": [0, 5, 3, 1, 2, 6, 2, 1, 4, 3, 5],
+    "cost_mean": 9.933333333333334,
+    "cost_standard_deviation": 3.785351884420904,
+    "tiles": [
+        [0, 1, 3, 6],
+        [0, 1, 4, 7],
+        [0, 1, 4, 8],
+        [0, 2, 5, 9],
+        [0, 2, 5, 10]
+    ],
+    "costs": [
+        [8, 8, 8, 8, 8, 8],
+        [3, 8, 8, 8, 8, 8],
+        [6, 4, 11, 11, 11, 11],
+        [12, 12, 12, 12, 12, 12],
+        [14, 14, 14, 5, 14, 14]
+        ]
+    }
+    maker = InstanceMaker(
+        height=the_dict["height"],
+        tile_min_size=2,
+        common_symbol_max_count=1,
+        desired_cost_mean=9.933333333333334,
+        LB_standard_deviation=3,
+        UB_standard_deviation=4)
+    maker.node_count = the_dict["symbol_count"]
+
+    the_instance = Instance(the_dict)
+
+    p_i_occurrences = [5, 3, 8, 1, 4, 8, 1, 2, 3, 4, 5]
+    coeff_for_quadratic = [
+            [5, 6, 4, 2, 4, 4, 2, 2, 2, 2, 2],
+            [0, 3, 0, 2, 4, 0, 2, 2, 2, 0, 0],
+            [0, 0, 8, 0, 0, 16, 0, 0, 0, 8, 8],
+            [0, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 0, 4, 0, 0, 4, 4, 0, 0],
+            [0, 0, 0, 0, 0, 8, 0, 0, 0, 8, 8],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5]
+        ]
+    computed_weights = maker.create_weight_according_to_mean_and_deviation(the_instance, p_i_occurrences, coeff_for_quadratic)
+    weights =  [computed_weights[i] for i in range(maker.node_count)]
+
+    assert weights == [1.3143362332957655,
+                        3.3797392557243984,
+                        6.145356839344423,
+                        1.1308671600950275,
+                        4.2333493169300365,
+                        6.145439612266686,
+                        1.130865974019982,
+                        2.110105630479951,
+                        3.1299100560926725,
+                        1.6543612251060553e-24,
+                        0.23153152447721043]
